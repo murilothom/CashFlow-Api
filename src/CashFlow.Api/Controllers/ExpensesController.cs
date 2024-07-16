44737fd48a5295d6a631cda2aeb74a42a4cf56c8
Murilo Thom <murilothom.2002@hotmail.com>
2024-07-16 14:50:29 -0300

    Register expense controller

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterExpenseDto request)
+        {
+            return Created();
+        }
+    }
+}
